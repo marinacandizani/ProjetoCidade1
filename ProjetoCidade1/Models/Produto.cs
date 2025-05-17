702b9153646a608863d5fa07b71d9932d9e563aa
marinacandizani <msc.candizani@gmail.com>
2025-05-16 23:37:09 -0300

    Criando as Models

---
+++ ProjetoCidade1/Models/Produto.cs
@@ -0,0 +1,11 @@
+﻿namespace ProjetoCidade1.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public decimal Preco { get; set; }
+        public int quantidade { get; set; }
+    }
+}
---
+++ ProjetoCidade1/Models/Usuário.cs
@@ -0,0 +1,10 @@
+﻿namespace ProjetoCidade1.Models
+{
+    public class Usuário
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+    }
+}
